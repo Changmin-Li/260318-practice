d48f4a788314ecbb47180e6c085a0802e0c670c8
Changmin Lee <geochangmin@gmail.com>
2026-03-18 04:16:58 +0000

    자기 소개 페이지 소개 내용 수정: '대학생'에서 '재학 대학생'으로 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.header("이름")
 st.write("이창민")
 
 st.header("소개")
-st.write("인천대학교 사범대학 수학교육과 2학년 대학생")
+st.write("인천대학교 사범대학 수학교육과 2학년 재학 대학생")
 
 st.header("기술 스택")
 st.write("- LaTeX\n- MATLAB\n- Python\n- C")
